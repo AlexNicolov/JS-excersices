8682f0871fa64add260ea077861998d1b0f2eec3
AlexNicolov <111065673+AlexNicolov@users.noreply.github.com>
2022-11-24 16:06:50 +0200

    yes
    yes

--- ArraysAdvancedExercise/sortArrayByTwoCriteria.js
+++
@@ -1,7 +0,0 @@
-function sortArrayByTwoCriteria(params) {
-    
-    
-}
-
-sortArrayByTwoCriteria(['alpha', 'beta', 'gamma']);
-sortArrayByTwoCriteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
