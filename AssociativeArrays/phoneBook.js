7838c6c41b9fb1bdc90fdd6d7ec431b8fefb0cd2
Alex Nikolov <alnikolov77@gmail.com>
2022-11-26 10:49:15 +0200

    phoneBook

---
+++ AssociativeArrays/phoneBook.js
@@ -0,0 +1,20 @@
+function phoneBook(input) {
+    let result = {};
+
+    for (let line of input) {
+        let token = line.split(' ');
+        let name = token[0];
+        let phone = token[1];
+
+        result[name] = phone;
+    }
+for (let name in result) {
+   console.log(`${name} -> ${result[name]}`);
+    }
+}
+
+
+phoneBook(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344'])
