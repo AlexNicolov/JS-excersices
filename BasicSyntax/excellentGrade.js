dfd14d8bb4608e405c61f301ec82183bfbfb9e0e
Alex Nikolov <alnikolov77@gmail.com>
2022-11-28 16:48:39 +0200

    excellentGrade

---
+++ BasicSyntax/excellentGrade.js
@@ -0,0 +1,8 @@
+function excellentGrade(grade) {
+    console.log(grade >= 5.50 ? "Excellent" : "Not excellent");
+}
+
+excellentGrade(5.50);
+excellentGrade(4.35);
+excellentGrade(3.26);
+excellentGrade(6.00);
