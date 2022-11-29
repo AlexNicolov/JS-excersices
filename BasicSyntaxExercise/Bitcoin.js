a2a481df92036fa16a13ab340a610a2505d36b08
Alex Nikolov <alnikolov77@gmail.com>
2022-11-29 15:49:15 +0200

    cripto

---
+++ BasicSyntaxExercise/Bitcoin.js
@@ -0,0 +1,34 @@
+function solve(array) {
+
+    let bitcoinPrice = 11949.16;
+    let goldPriceGram = 67.51;
+    let bitcoinNumber = 0;
+    let goldInGrams = 0;
+    let days = 0;
+    let sum = 0;
+
+    for (let i = 1; i <= array.length; i++) {
+        days++;
+        let gold1 = 100
+sum = gold1 * goldPriceGram
+if (sum >= bitcoinPrice) {
+    bitcoinNumber += 1;
+
+}
+            
+        }
+
+
+    
+
+
+    console.log(sum);
+}
+
+
+
+
+
+solve([100, 200, 300])
+// solve([50, 100]);
+// solve([3124.15, 504.212, 2511.124]);
