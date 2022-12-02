3ff0bfc21370b7d181bd26b64aa6f0cf31d452ba
Alex Nikolov <alnikolov77@gmail.com>
2022-12-02 13:15:47 +0200

    townInfo

---
+++ DataTypesAndVariablesExercise/townInfo.js
@@ -0,0 +1,13 @@
+function townInfo(char1, char2, char3) {
+    let town = char1;
+    let population = char2;
+    let area = char3;
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+
+townInfo('Sofia',
+1286383,
+492);
+townInfo('Plovdiv',
+1481353,
+512);
