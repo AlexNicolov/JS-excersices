4e5cc1c757eacc6c7669409b761aae4c0bdf01a2
Alex Nikolov <alnikolov77@gmail.com>
2022-12-03 13:11:56 +0200

    smallestNum2

---
+++ FunctionsExercise/smallestNum2.js
@@ -0,0 +1,8 @@
+function smallestNumber(num1, num2, num3) {
+
+    let smallNum = (num1, num2, num3) => Math.min(num1, num2, num3);
+    console.log(smallNum(num1, num2, num3));
+}
+
+smallestNumber(2, 5, 3)
+smallestNumber(600, 342, 123)
