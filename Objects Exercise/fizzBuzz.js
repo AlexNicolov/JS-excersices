f6992d6c6c0b8a9d58414019f634135c141056f2
Alex Nikolov <alnikolov77@gmail.com>
2022-12-05 17:07:28 +0200

    fizzBuzz

--- Objects Exercise/fizzBuzz.js
+++ Objects Exercise/fizzBuzz.js
@@ -1,19 +1,15 @@
 function fizzBuzz(input) {
-
-    for (let z = 1; z <= input; z++){
-    
-
-        if(z % 3 == 0 & z % 5 == 0){
+    for (let z = 1; z <= input; z++) {
+        if (z % 3 == 0 & z % 5 == 0) {
             console.log('fizzBuzz')
-        }else if (z % 3 == 0) {
-        console.log('Fizz')
-        }else if (z % 5 == 0 ) {
-        console.log('Buzz')
-        }else{
-        console.log(z)
+        } else if (z % 3 == 0) {
+            console.log('Fizz')
+        } else if (z % 5 == 0) {
+            console.log('Buzz')
+        } else {
+            console.log(z)
         }
-        }
-        
     }
+}
 
 fizzBuzz('15')
