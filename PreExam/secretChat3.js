b828ee2ec7d4097410e5a885c091a571967327a3
Alex Nikolov <alnikolov77@gmail.com>
2022-12-06 14:14:20 +0200

    secretChat3

---
+++ PreExam/secretChat3.js
@@ -0,0 +1,33 @@
+function secretChat3(input) {
+ let   text = input.reverse().join(', ')
+
+//    for (let index = 0; index < input.length; index++) {
+//     text.unshift(input[index])
+
+
+
+// }
+// console.log(text.join(' '));
+    
+
+console.log(text);
+
+
+
+}
+
+secretChat3([
+    'heVVodar!gniV',
+    'ChangeAll:|:V:|:l',
+    'Reverse:|:!gnil',
+    'InsertSpace:|:5',
+    'Reveal']);
+    console.log('---');
+// secretChat3([
+//     'Hiware?uiy',
+//     'ChangeAll:|:i:|:o',
+//     'Reverse:|:?uoy',
+//     'Reverse:|:jd',
+//     'InsertSpace:|:3',
+//     'InsertSpace:|:7',
+//     'Reveal']);
