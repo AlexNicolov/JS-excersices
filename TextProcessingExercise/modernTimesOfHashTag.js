8e87f5e0b45d2e1cce2907761a081616e938ed2d
Alex Nikolov <alnikolov77@gmail.com>
2022-12-09 15:59:14 +0200

    modernTimesOfHashTag

---
+++ TextProcessingExercise/modernTimesOfHashTag.js
@@ -0,0 +1,17 @@
+function solve(input) {
+  let words = input.split(" ")
+
+  for (let word of words) {
+      if(word.startsWith("#") && word.length > 1){
+       let asciiCode = word.charCodeAt(1);
+        let isLetter = (asciiCode >= 65 && asciiCode <= 90) ||  (asciiCode >= 97 && asciiCode <= 122)
+        if(isLetter){
+            console.log(word.substring(1));
+        }
+        }
+    }
+}
+
+solve('Nowadays everyone uses # to tag a #special word in #socialMedia');	
+console.log('---');
+solve('The symbol # is known #variously in English-speaking #regi123ons as the #number sign');
