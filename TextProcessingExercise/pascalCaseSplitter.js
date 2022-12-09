46d91453b61cd5cae7215b37e96845fa3f9f98d5
Alex Nikolov <alnikolov77@gmail.com>
2022-12-09 16:39:15 +0200

    pascalCaseSplitter

---
+++ TextProcessingExercise/pascalCaseSplitter.js
@@ -0,0 +1,19 @@
+function pascalCaseSplitter(input) {
+    let words = [];
+    let currWord = input[0];
+
+        for (let index = 1; index < input.length; index++) {
+if(input[index].toUpperCase() != input[index]){
+    currWord = currWord.concat(input[index]);
+}else{
+    words.push(currWord);
+    currWord = input[index];
+}
+        }
+ words.push(currWord);
+        console.log(words.join(', '));
+}
+
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
+pascalCaseSplitter('HoldTheDoor');
+pascalCaseSplitter('ThisIsSoAnnoyingToDo');
